9ad7b20a64f6dfe42aaf54981984c2931b801a26
Jonathan Ellis <jbellis@datastax.com>
2023-08-31 22:08:06 -0500

    1.2 is the sweet spot for neighborOverflow; see tests on #33

--- src/main/java/com/github/jbellis/jvector/example/Bench.java
+++ src/main/java/com/github/jbellis/jvector/example/Bench.java
@@ -41,7 +41,7 @@ public class Bench {
         var topK = ds.groundTruth.get(0).size();
 
         var start = System.nanoTime();
-        var builder = new GraphIndexBuilder<>(floatVectors, VectorEncoding.FLOAT32, ds.similarityFunction, M, efConstruction, 1.5f, 1.4f);
+        var builder = new GraphIndexBuilder<>(floatVectors, VectorEncoding.FLOAT32, ds.similarityFunction, M, efConstruction, 1.2f, 1.4f);
         var index = builder.build();
         long buildNanos = System.nanoTime() - start;
 
