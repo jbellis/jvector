a7b0e7bb4f2599a2761bdc891229e43f055a2e03
Jonathan Ellis <jbellis@datastax.com>
2023-09-12 15:50:53 -0500

    Use 10% of points in PQ

--- jvector-base/src/main/java/com/github/jbellis/jvector/pq/ProductQuantization.java
+++ jvector-base/src/main/java/com/github/jbellis/jvector/pq/ProductQuantization.java
@@ -22,6 +22,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
