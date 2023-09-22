f8107060ed32525a9106dc964b8aeaf5cd547491
Jonathan Ellis <jbellis@datastax.com>
2023-09-22 17:17:55 -0500

    Bench back to DP

--- jvector-examples/src/main/java/io/github/jbellis/jvector/example/Bench.java
+++ jvector-examples/src/main/java/io/github/jbellis/jvector/example/Bench.java
@@ -169,7 +169,7 @@ public class Bench {
         var queryVectors = SiftLoader.readFvecs("fvec/pages_ada_002_100k_query_vectors_10k.fvec").subList(0, 10_000);
         var gt = SiftLoader.readIvecs("fvec/pages_ada_002_100k_indices_query_vectors_10k.ivec").subList(0, 10_000);
         var ds = new DataSet("wikipedia",
-                             VectorSimilarityFunction.COSINE,
+                             VectorSimilarityFunction.DOT_PRODUCT,
                              baseVectors,
                              queryVectors,
                              gt);
