6736bd7d2befe21d6e6437f5e38049a9aa1341c6
Jonathan Ellis <jbellis@datastax.com>
2023-11-10 12:34:20 -0600

    trying w/ top 1 recall.  doesn't seem sensitive enough -- too easy to find top 1 and/or at small K the OQ factor is too strong

--- jvector-base/src/main/java/io/github/jbellis/jvector/pq/VectorCompressor.java
+++ jvector-base/src/main/java/io/github/jbellis/jvector/pq/VectorCompressor.java
@@ -16,9 +16,19 @@
 
 package io.github.jbellis.jvector.pq;
 
+import io.github.jbellis.jvector.graph.GraphIndex;
+import io.github.jbellis.jvector.graph.GraphSearcher;
+import io.github.jbellis.jvector.graph.ListRandomAccessVectorValues;
+import io.github.jbellis.jvector.graph.NodeSimilarity;
+import io.github.jbellis.jvector.graph.RandomAccessVectorValues;
+import io.github.jbellis.jvector.util.Bits;
+import io.github.jbellis.jvector.vector.VectorEncoding;
+import io.github.jbellis.jvector.vector.VectorSimilarityFunction;
+
 import java.io.DataOutput;
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Interface for vector compression.  T is the encoded (compressed) vector type;
@@ -37,4 +47,48 @@ public interface VectorCompressor<T> {
      *                          without committing to a specific type T.
      */
     CompressedVectors createCompressedVectors(Object[] compressedVectors);
+
+    static VectorCompressor<?> guessCompressorFor(GraphIndex<float[]> index, List<float[]> vectors, VectorSimilarityFunction similarityFunction) {
+        var R = ThreadLocalRandom.current();
+        var ravv = new ListRandomAccessVectorValues(vectors, vectors.get(0).length);
+
+        // first, find the 1@K recall for uncompressed queries
+        int K = 4;
+        var baseRecall = 0.0;
+        int N_QUERIES = 200;
+        for (int i = 0; i < N_QUERIES; i++) {
+            int qOrd = R.nextInt(ravv.size());
+            var qv = ravv.vectorValue(qOrd);
+            var sr = GraphSearcher.search(qv, K, ravv, VectorEncoding.FLOAT32, similarityFunction, index, Bits.ALL);
+            baseRecall += sr.getNodes()[0].node == qOrd ? 1 : 0;
+        }
+        System.out.printf("recall for uncompressed queries: %.2f%n", baseRecall / N_QUERIES);
+
+        // see if we can get BQ to work
+        var bq = BinaryQuantization.compute(ravv);
+        var bqVectors = bq.encodeAll(vectors);
+        var cv = bq.createCompressedVectors(bqVectors);
+        for (int oq = 3; oq <= 5; oq++) {
+            var bqRecall = 0.0;
+            for (int i = 0; i < N_QUERIES; i++) {
+                int qOrd = R.nextInt(ravv.size());
+                var qv = ravv.vectorValue(qOrd);
+
+                var view = index.getView();
+                NodeSimilarity.ApproximateScoreFunction sf = cv.approximateScoreFunctionFor(qv, similarityFunction);
+                NodeSimilarity.ReRanker<float[]> rr = (j, vv) -> similarityFunction.compare(qv, vv.get(j));
+                var sr = new GraphSearcher.Builder<>(view)
+                        .build()
+                        .search(sf, rr, oq * K, Bits.ALL);
+                bqRecall += sr.getNodes()[0].node == qOrd ? 1 : 0;
+            }
+            System.out.printf("recall for bq oq=%s queries: %.2f%n", oq, bqRecall / N_QUERIES);
+            if (bqRecall >= 0.99 * baseRecall) {
+                return bq;
+            }
+        }
+
+        System.out.println("no good bq found");
+        return null;
+    }
 }
