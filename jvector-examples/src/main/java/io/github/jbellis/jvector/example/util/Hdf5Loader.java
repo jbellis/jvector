9c79fa43ba1d5057ca80a034ddf053e469f446ed
Joel Knighton <joel.knighton@datastax.com>
2023-11-20 14:44:16 -0600

    Scrub both fvec/ivec and hdf5 dot product datasets

--- jvector-examples/src/main/java/io/github/jbellis/jvector/example/util/Hdf5Loader.java
+++ jvector-examples/src/main/java/io/github/jbellis/jvector/example/util/Hdf5Loader.java
@@ -17,14 +17,11 @@
 package io.github.jbellis.jvector.example.util;
 
 import io.github.jbellis.jvector.vector.VectorSimilarityFunction;
-import io.github.jbellis.jvector.vector.VectorUtil;
 import io.jhdf.HdfFile;
 import io.jhdf.api.Dataset;
-import io.jhdf.object.datatype.DataType;
 import io.jhdf.object.datatype.FloatingPoint;
 
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.IntStream;
 
@@ -47,8 +44,8 @@ public class Hdf5Loader {
         // read the data
         float[][] baseVectors;
         float[][] queryVectors;
-        int[][] groundTruth;
         Path path = Path.of(HDF5_DIR).resolve(filename);
+        var gtSets = new ArrayList<HashSet<Integer>>();
         try (HdfFile hdf = new HdfFile(path)) {
             baseVectors = (float[][]) hdf.getDatasetByPath("train").getData();
             Dataset queryDataset = hdf.getDatasetByPath("test");
@@ -65,78 +62,17 @@ public class Hdf5Loader {
             } else {
                 queryVectors = (float[][]) queryDataset.getData();
             }
-            groundTruth = (int[][]) hdf.getDatasetByPath("neighbors").getData();
-        }
-
-        List<float[]> scrubbedBaseVectors;
-        List<float[]> scrubbedQueryVectors;
-        List<Set<Integer>> gtSet;
-        if (similarityFunction == VectorSimilarityFunction.DOT_PRODUCT) {
-            // verify that vectors are normalized and sane.
-            // this is necessary b/c NYT dataset contains zero vectors (!)
-            scrubbedBaseVectors = new ArrayList<>(baseVectors.length);
-            scrubbedQueryVectors = new ArrayList<>(queryVectors.length);
-            gtSet = new ArrayList<>(groundTruth.length);
-            // remove zero vectors, noting that this will change the indexes of the ground truth answers
-            Map<Integer, Integer> rawToScrubbed = new HashMap<>();
-            {
-                int j = 0;
-                for (int i = 0; i < baseVectors.length; i++) {
-                    float[] v = baseVectors[i];
-                    if (Math.abs(normOf(v)) > 1e-5) {
-                        scrubbedBaseVectors.add(v);
-                        rawToScrubbed.put(i, j++);
-                    }
+            int[][] groundTruth = (int[][]) hdf.getDatasetByPath("neighbors").getData();
+            gtSets = new ArrayList<>(groundTruth.length);
+            for (int[] i : groundTruth) {
+                var gtSet = new HashSet<Integer>(i.length);
+                for (int j : i) {
+                    gtSet.add(j);
                 }
-            }
-            for (int i = 0; i < queryVectors.length; i++) {
-                float[] v = queryVectors[i];
-                if (Math.abs(normOf(v)) > 1e-5) {
-                    scrubbedQueryVectors.add(v);
-                    var gt = new HashSet<Integer>();
-                    for (int j = 0; j < groundTruth[i].length; j++) {
-                        gt.add(rawToScrubbed.get(groundTruth[i][j]));
-                    }
-                    gtSet.add(gt);
-                }
-            }
-            // now that the zero vectors are removed, we can normalize
-            if (Math.abs(normOf(baseVectors[0]) - 1.0) > 1e-5) {
-                normalizeAll(scrubbedBaseVectors);
-                normalizeAll(scrubbedQueryVectors);
-            }
-            assert scrubbedQueryVectors.size() == gtSet.size();
-        }
-        else {
-            scrubbedBaseVectors = Arrays.asList(baseVectors);
-            scrubbedQueryVectors = Arrays.asList(queryVectors);
-            gtSet = new ArrayList<>(groundTruth.length);
-            for (int[] gt : groundTruth) {
-                var gtSetForQuery = new HashSet<Integer>();
-                for (int i : gt) {
-                    gtSetForQuery.add(i);
-                }
-                gtSet.add(gtSetForQuery);
+                gtSets.add(gtSet);
             }
         }
 
-        System.out.format("%n%s: %d base and %d query vectors loaded, dimensions %d%n",
-                          filename, scrubbedBaseVectors.size(), scrubbedQueryVectors.size(), scrubbedBaseVectors.get(0).length);
-
-        return new DataSet(path.getFileName().toString(), similarityFunction, scrubbedBaseVectors, scrubbedQueryVectors, gtSet);
-    }
-
-    private static void normalizeAll(Iterable<float[]> vectors) {
-        for (float[] v : vectors) {
-            VectorUtil.l2normalize(v);
-        }
-    }
-
-    private static float normOf(float[] baseVector) {
-        float norm = 0;
-        for (float v : baseVector) {
-            norm += v * v;
-        }
-        return (float) Math.sqrt(norm);
+        return DataSet.getScrubbedDataSet(path.getFileName().toString(), similarityFunction, Arrays.asList(baseVectors), Arrays.asList(queryVectors), gtSets);
     }
 }
