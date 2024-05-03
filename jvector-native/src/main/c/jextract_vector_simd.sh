fc592561d86e2a002514e8104242c34576fdcc8d
Joel Knighton <joel.knighton@datastax.com>
2024-05-03 11:26:35 -0500

    Check for GCC 11+ instead of failing compile

--- jvector-native/src/main/c/jextract_vector_simd.sh
+++ jvector-native/src/main/c/jextract_vector_simd.sh
@@ -21,18 +21,32 @@ mkdir -p ../resources
 # compile jvector_simd_check.c as x86-64
 # compile jvector_simd.c as skylake-avx512
 # produce one shared library
-gcc -fPIC -O3 -march=icelake-server -c jvector_simd.c -o jvector_simd.o
-gcc -fPIC -O3 -march=x86-64 -c jvector_simd_check.c -o jvector_simd_check.o
-gcc -shared -o ../resources/libjvector.so jvector_simd_check.o jvector_simd.o
 
+# Desired minimum GCC version
+MIN_GCC_VERSION=11
 
+# Check if GCC is installed
+if ! command -v gcc &> /dev/null; then
+    echo "GCC is not installed. Please install GCC 11+ to build supporting native libraries."
+else
+    CURRENT_GCC_VERSION=$(gcc -dumpversion)
+
+    # Check if the current GCC version is greater than or equal to the minimum required version
+    if [ "$(printf '%s\n' "$MIN_GCC_VERSION" "$CURRENT_GCC_VERSION" | sort -V | head -n1)" = "$MIN_GCC_VERSION" ]; then
+        gcc -fPIC -O3 -march=icelake-server -c jvector_simd.c -o jvector_simd.o
+        gcc -fPIC -O3 -march=x86-64 -c jvector_simd_check.c -o jvector_simd_check.o
+        gcc -shared -o ../resources/libjvector.so jvector_simd_check.o jvector_simd.o
+    else
+        echo "WARNING: GCC version $CURRENT_GCC_VERSION is too old. Please upgrade to GCC $MIN_GCC_VERSION or newer."
+    fi
+fi
 
 # Generate Java source code
 # Should only be run when c header changes
 # Check if jextract is available before running
 if ! command -v jextract &> /dev/null
 then
-    echo "jextract could not be found, please install it if you need to update bindings."
+    echo "WARNING: jextract could not be found, please install it if you need to update bindings."
     exit 0
 fi
 
