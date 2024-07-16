6f05cab3f6324c2d317b88c95f10f2a2c7a2bda5
Jonathan Ellis <jbellis@datastax.com>
2024-07-16 08:43:25 -0500

    fix rerankerFor

--- jvector-base/src/main/java/io/github/jbellis/jvector/graph/disk/OnDiskGraphIndex.java
+++ jvector-base/src/main/java/io/github/jbellis/jvector/graph/disk/OnDiskGraphIndex.java
@@ -35,6 +35,7 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.file.Path;
 import java.util.EnumMap;
+import java.util.EnumSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -260,11 +261,12 @@ public class OnDiskGraphIndex implements GraphIndex, AutoCloseable, Accountable
             reader.close();
         }
 
+        // TODO permissibleFeatures is never anything but "all features"?
         public ScoreFunction.ExactScoreFunction rerankerFor(VectorFloat<?> queryVector, VectorSimilarityFunction vsf, Set<FeatureId> permissibleFeatures) {
             if (permissibleFeatures.contains(FeatureId.LVQ) && features.containsKey(FeatureId.LVQ)) {
                 return ((LVQ) features.get(FeatureId.LVQ)).rerankerFor(queryVector, vsf, this);
             } else if (permissibleFeatures.contains(FeatureId.INLINE_VECTORS) && features.containsKey(FeatureId.INLINE_VECTORS)) {
-                return rerankerFor(queryVector, vsf);
+                return RandomAccessVectorValues.super.rerankerFor(queryVector, vsf);
             } else {
                 throw new UnsupportedOperationException("No reranker available for this graph");
             }
@@ -272,10 +274,7 @@ public class OnDiskGraphIndex implements GraphIndex, AutoCloseable, Accountable
 
         @Override
         public ScoreFunction.ExactScoreFunction rerankerFor(VectorFloat<?> queryVector, VectorSimilarityFunction vsf) {
-            if (!features.containsKey(FeatureId.INLINE_VECTORS)) {
-                throw new UnsupportedOperationException("No inline vectors in this graph");
-            }
-            return RandomAccessVectorValues.super.rerankerFor(queryVector, vsf);
+            return rerankerFor(queryVector, vsf, EnumSet.allOf(FeatureId.class));
         }
 
         @Override
