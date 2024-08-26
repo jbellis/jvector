0c1b52c064c538e6e5eb390b2f681c55eb2e1cc9
Jonathan Ellis <jbellis@datastax.com>
2024-08-26 10:19:38 -0500

    setup for C10M

--- jvector-examples/src/main/java/io/github/jbellis/jvector/example/Bench.java
+++ jvector-examples/src/main/java/io/github/jbellis/jvector/example/Bench.java
@@ -42,17 +42,16 @@ public class Bench {
     public static void main(String[] args) throws IOException {
         System.out.println("Heap space available is " + Runtime.getRuntime().maxMemory());
 
-        var mGrid = List.of(32); // List.of(16, 24, 32, 48, 64, 96, 128);
-        var efConstructionGrid = List.of(100); // List.of(60, 80, 100, 120, 160, 200, 400, 600, 800);
-        var efSearchGrid = List.of(1.0, 2.0);
+        var mGrid = List.of(32, 48, 64, 96, 128); // List.of(16, 24, 32, 48, 64, 96, 128);
+        var efConstructionGrid = List.of(100, 200, 300); // List.of(60, 80, 100, 120, 160, 200, 400, 600, 800);
+        var efSearchGrid = List.of(1.0, 1.2, 1.5, 2.0);
         List<Function<DataSet, CompressorParameters>> buildCompression = Arrays.asList(
-                ds -> new PQParameters(ds.getDimension() / 8, 256, ds.similarityFunction == VectorSimilarityFunction.EUCLIDEAN, UNWEIGHTED),
                 __ -> CompressorParameters.NONE
         );
         List<Function<DataSet, CompressorParameters>> searchCompression = Arrays.asList(
                 __ -> CompressorParameters.NONE,
                 // ds -> new CompressorParameters.BQParameters(),
-                ds -> new PQParameters(ds.getDimension() / 8, 256, ds.similarityFunction == VectorSimilarityFunction.EUCLIDEAN, UNWEIGHTED)
+                ds -> new PQParameters(ds.getDimension() / 4, 256, ds.similarityFunction == VectorSimilarityFunction.EUCLIDEAN, UNWEIGHTED)
         );
         List<EnumSet<FeatureId>> featureSets = Arrays.asList(
                 EnumSet.of(FeatureId.INLINE_VECTORS),
@@ -66,50 +65,8 @@ public class Bench {
         var pattern = Pattern.compile(regex);
 
         // large embeddings calculated by Neighborhood Watch.  100k files by default; 1M also available
-        var coreFiles = List.of(
-                "ada002-100k",
-                "cohere-english-v3-100k",
-                "openai-v3-small-100k",
-                "nv-qa-v4-100k",
-                "colbert-1M",
-                "gecko-100k");
+        var coreFiles = List.of("colbert-10M");
         executeNw(coreFiles, pattern, buildCompression, featureSets, searchCompression, mGrid, efConstructionGrid, efSearchGrid);
-
-        var extraFiles = List.of(
-                "openai-v3-large-3072-100k",
-                "openai-v3-large-1536-100k",
-                "e5-small-v2-100k",
-                "e5-base-v2-100k",
-                "e5-large-v2-100k");
-        executeNw(extraFiles, pattern, buildCompression, featureSets, searchCompression, mGrid, efConstructionGrid, efSearchGrid);
-
-        // smaller vectors from ann-benchmarks
-        var hdf5Files = List.of(
-                // large files not yet supported
-                // "hdf5/deep-image-96-angular.hdf5",
-                // "hdf5/gist-960-euclidean.hdf5",
-                "glove-25-angular.hdf5",
-                "glove-50-angular.hdf5",
-                "lastfm-64-dot.hdf5",
-                "glove-100-angular.hdf5",
-                "glove-200-angular.hdf5",
-                "nytimes-256-angular.hdf5",
-                "sift-128-euclidean.hdf5");
-        for (var f : hdf5Files) {
-            if (pattern.matcher(f).find()) {
-                DownloadHelper.maybeDownloadHdf5(f);
-                Grid.runAll(Hdf5Loader.load(f), mGrid, efConstructionGrid, featureSets, buildCompression, searchCompression, efSearchGrid);
-            }
-        }
-
-        // 2D grid, built and calculated at runtime
-        if (pattern.matcher("2dgrid").find()) {
-            searchCompression = Arrays.asList(__ -> CompressorParameters.NONE,
-                                              ds -> new PQParameters(ds.getDimension(), 256, true, UNWEIGHTED));
-            buildCompression = Arrays.asList(__ -> CompressorParameters.NONE);
-            var grid2d = DataSetCreator.create2DGrid(4_000_000, 10_000, 100);
-            Grid.runAll(grid2d, mGrid, efConstructionGrid, featureSets, buildCompression, searchCompression, efSearchGrid);
-        }
     }
 
     private static void executeNw(List<String> coreFiles, Pattern pattern, List<Function<DataSet, CompressorParameters>> buildCompression, List<EnumSet<FeatureId>> featureSets, List<Function<DataSet, CompressorParameters>> compressionGrid, List<Integer> mGrid, List<Integer> efConstructionGrid, List<Double> efSearchGrid) throws IOException {
