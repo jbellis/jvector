114130342eb4e6bb15b68b2ecb0e884113157b0b
Mariano Tepper <mariano.tepper@datastax.com>
2024-10-28 12:16:22 -0700

    Add a few more tests for NESOptimizer

--- jvector-tests/src/test/java/io/github/jbellis/jvector/optimization/TestNaturalEvolutionStrategies.java
+++ jvector-tests/src/test/java/io/github/jbellis/jvector/optimization/TestNaturalEvolutionStrategies.java
@@ -31,9 +31,53 @@ public class TestNaturalEvolutionStrategies {
         var sol = xnes.optimize(loss, initialSolution, 0.5);
 
         assertTrue("error=" + sol.error + " tolerance=" + tolerance, sol.error <= tolerance);
-        assertTrue("sol.x[0]=" + sol.x[0], sol.x[0] < 1e-4);
-        assertTrue("sol.x[1]=" + sol.x[1], sol.x[1] < 1e-4);
+        assertTrue("sol.x[0]=" + sol.x[0] + " sol.x[1]=" + sol.x[1], sol.x[0] < 1e-3 && sol.x[1] < 1e-3);
+    }
 
+    @Test
+    public void testQuarticOptimization() {
+        class TestLossFunction extends LossFunction {
+            public TestLossFunction(int nDims) {
+                super(nDims);
+            }
+
+            public double compute(double[] x) {
+                return -1 * DoubleStream.of(x).map(elem -> Math.pow(elem, 4) - Math.pow(elem, 2) - 0.25 * elem).sum();
+            }
+        }
+        var loss = new TestLossFunction(2);
+        loss.setMinBounds(new double[] {-1000, -1000});
+        loss.setMaxBounds(new double[] {1000, 1000});
+
+
+        var xnes = new NESOptimizer(NESOptimizer.Distribution.SEPARABLE);
+
+        var tolerance = 1e-9;
+        xnes.setTol(tolerance);
+
+        {
+            // Setting the initial solution close to the global minimum works
+            var sol = xnes.optimize(loss, new double[] {1, 1}, 0.5);
+
+            assertTrue("error=" + sol.error + " tolerance=" + tolerance, sol.error <= tolerance);
+            assertTrue("sol.x[0]=" + sol.x[0] + " sol.x[1]=" + sol.x[1], Math.abs(sol.x[0] - 0.76284314) < 1e-3 && Math.abs(sol.x[1] - 0.76284314) < 1e-3);
+        }
+
+        {
+            // Setting the initial solution close to a local minimum makes it fail
+            var sol = xnes.optimize(loss, new double[]{-1, -1}, 0.2);
+
+            assertTrue("error=" + sol.error + " tolerance=" + tolerance, sol.error <= tolerance);
+            assertTrue("sol.x[0]=" + sol.x[0] + " sol.x[1]=" + sol.x[1], Math.abs(sol.x[0] - 0.76284314) >= 1e-3 || Math.abs(sol.x[1] - 0.76284314) >= 1e-3);
+        }
+
+        {
+            // Increasing the variance of for the natural gradient makes it work again
+            var sol = xnes.optimize(loss, new double[] {-1, -1}, 1);
+
+            assertTrue("error=" + sol.error + " tolerance=" + tolerance, sol.error <= tolerance);
+            assertTrue("sol.x[0]=" + sol.x[0] + " sol.x[1]=" + sol.x[1], Math.abs(sol.x[0] - 0.76284314) < 1e-3 && Math.abs(sol.x[1] - 0.76284314) < 1e-3);
+            }
 
     }
 }
