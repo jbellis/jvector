0fa72592f523704d868279bbbd79c9e18d8005ad
Mariano Tepper <mariano.tepper@datastax.com>
2024-11-04 14:09:38 -0800

    Fix access modifier of the fields in QuantizedSubVector

--- jvector-base/src/main/java/io/github/jbellis/jvector/pq/NVQuantization.java
+++ jvector-base/src/main/java/io/github/jbellis/jvector/pq/NVQuantization.java
@@ -384,10 +384,10 @@ public class NVQuantization implements VectorCompressor<NVQuantization.Quantized
      * A NuVeQ sub-vector.
      */
     public static class QuantizedSubVector {
-        private final ByteSequence<?> bytes;
-        private BitsPerDimension bitsPerDimension;
-        private final float kumaraswamyA;
-        private final float kumaraswamyB;
+        public final ByteSequence<?> bytes;
+        public BitsPerDimension bitsPerDimension;
+        public final float kumaraswamyA;
+        public final float kumaraswamyB;
         public final float kumaraswamyBias;
         public final float kumaraswamyScale;
 
