8e6e47eafe86e1b14ff2e22c6410a72fc81c73f9
Mariano Tepper <mariano.tepper@datastax.com>
2024-11-28 14:48:56 -0800

    Refactor kumaraswamy benchmarks into their own package

--- jvector-twenty/src/main/java/bench/kumaraswamyApproximationSIMD.java
+++ jvector-twenty/src/main/java/bench/kumaraswamyApproximationSIMD.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package io.github.jbellis.jvector.vector;
+package bench;
 
 import jdk.incubator.vector.FloatVector;
 import jdk.incubator.vector.IntVector;
