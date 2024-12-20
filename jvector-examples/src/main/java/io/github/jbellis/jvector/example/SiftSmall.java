0092e8cfb8ff4b09a2896b5580b2d88abe0cc05a
Jonathan Ellis <jbellis@datastax.com>
2024-12-20 12:07:50 -0600

    r/m obsolete comment

--- jvector-examples/src/main/java/io/github/jbellis/jvector/example/SiftSmall.java
+++ jvector-examples/src/main/java/io/github/jbellis/jvector/example/SiftSmall.java
@@ -232,7 +232,7 @@ public class SiftSmall {
             // build the index vector-at-a-time (on disk)
             for (int ordinal = 0; ordinal < baseVectors.size(); ordinal++) {
                 VectorFloat<?> v = baseVectors.get(ordinal);
-                // compress the new vector and add it to the PQVectors (via incrementallyCompressedVectors)
+                // compress the new vector and add it to the PQVectors
                 pqv.encodeAndSet(ordinal, v);
                 // write the full vector to disk
                 writer.writeInline(ordinal, Feature.singleState(FeatureId.INLINE_VECTORS, new InlineVectors.State(v)));
