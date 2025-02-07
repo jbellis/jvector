2cf61bee1444e6c6ca0c1125c6f3ac6f2f6b84df
Jonathan Ellis <jbellis@datastax.com>
2025-02-07 17:38:48 -0600

    SparseIntMap

---
+++ jvector-base/src/main/java/io/github/jbellis/jvector/util/SparseIntMap.java
@@ -0,0 +1,60 @@
+package io.github.jbellis.jvector.util;
+
+import io.github.jbellis.jvector.graph.NodesIterator;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SparseIntMap<T> implements IntMap<T> {
+    private final ConcurrentHashMap<Integer, T> map;
+
+    public SparseIntMap() {
+        this.map = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public boolean compareAndPut(int key, T existing, T value) {
+        if (value == null) {
+            throw new IllegalArgumentException("compareAndPut() value cannot be null -- use remove() instead");
+        }
+
+        if (existing == null) {
+            T result = map.putIfAbsent(key, value);
+            return result == null;
+        }
+
+        return map.replace(key, existing, value);
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    public T get(int key) {
+        return map.get(key);
+    }
+
+    @Override
+    public T remove(int key) {
+        return map.remove(key);
+    }
+
+    @Override
+    public boolean containsKey(int key) {
+        return map.containsKey(key);
+    }
+
+    @Override
+    public NodesIterator keysIterator() {
+        var minSize = size();  // if keys are added concurrently we will miss them
+        var keysIterator = map.keySet().stream().mapToInt(Integer::intValue).iterator();
+        return NodesIterator.fromPrimitiveIterator(keysIterator, minSize);
+    }
+
+    @Override
+    public void forEach(IntBiConsumer<T> consumer) {
+        map.forEach(consumer::consume);
+    }
+}
--- jvector-tests/src/test/java/io/github/jbellis/jvector/util/TestIntMap.java
+++ jvector-tests/src/test/java/io/github/jbellis/jvector/util/TestIntMap.java
@@ -32,11 +32,15 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 
 @ThreadLeakScope(ThreadLeakScope.Scope.NONE)
-public class TestDenseIntMap extends RandomizedTest {
+public class TestIntMap extends RandomizedTest {
 
     @Test
     public void testInsert() {
-        var map = new DenseIntMap<String>(100);
+        testInsertInternal(new DenseIntMap<>(100));
+        testInsertInternal(new SparseIntMap<>());
+    }
+
+    private void testInsertInternal(IntMap<String> map) {
         for (int i = 0; i < 3; i++) {
             Assert.assertNull(map.get(i));
             Assert.assertFalse(map.containsKey(i));
@@ -50,7 +54,11 @@ public class TestDenseIntMap extends RandomizedTest {
 
     @Test
     public void testUpdate() {
-        var map = new DenseIntMap<String>(100);
+        testUpdateInternal(new DenseIntMap<>(100));
+        testUpdateInternal(new SparseIntMap<>());
+    }
+
+    private void testUpdateInternal(IntMap<String> map) {
         for (int i = 0; i < 3; i++) {
             map.compareAndPut(i, null, "value" + i);
         }
@@ -65,7 +73,11 @@ public class TestDenseIntMap extends RandomizedTest {
 
     @Test
     public void testRemove() {
-        var map = new DenseIntMap<String>(100);
+        testRemoveInternal(new DenseIntMap<>(100));
+        testRemoveInternal(new SparseIntMap<>());
+    }
+
+    private void testRemoveInternal(IntMap<String> map) {
         for (int i = 0; i < 3; i++) {
             map.compareAndPut(i, null, "value" + i);
         }
@@ -82,12 +94,12 @@ public class TestDenseIntMap extends RandomizedTest {
     @Test
     public void testConcurrency() throws InterruptedException {
         for (int i = 0; i < 100; i++) {
-            testConcurrencyOnce();
+            testConcurrencyOnce(new DenseIntMap<>(100));
+            testConcurrencyOnce(new SparseIntMap<>());
         }
     }
 
-    private static void testConcurrencyOnce() throws InterruptedException {
-        var map = new DenseIntMap<String>(100);
+    private static void testConcurrencyOnce(IntMap<String> map) throws InterruptedException {
         var source = new ConcurrentHashMap<Integer, String>();
 
         int nThreads = randomIntBetween(2, 16);
