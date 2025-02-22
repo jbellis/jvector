4100bf870cc6394d1fb23f50706f7d583f560c8a
Samuel Herman <sherman8915@gmail.com>
2025-02-21 23:07:52 -0800

    added a random benchamrk with parameter grid
    Signed-off-by: Samuel Herman <sherman8915@gmail.com>

--- benchmarks-jmh/src/main/java/io/github/jbellis/jvector/bench/StaticSetVectorsBenchmark.java
+++ benchmarks-jmh/src/main/java/io/github/jbellis/jvector/bench/StaticSetVectorsBenchmark.java
@@ -1,13 +1,27 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.jbellis.jvector.bench;
 
 import io.github.jbellis.jvector.example.SiftSmall;
+import io.github.jbellis.jvector.example.util.SiftLoader;
 import io.github.jbellis.jvector.graph.*;
 import io.github.jbellis.jvector.graph.similarity.BuildScoreProvider;
 import io.github.jbellis.jvector.util.Bits;
 import io.github.jbellis.jvector.vector.VectorSimilarityFunction;
-import io.github.jbellis.jvector.vector.VectorizationProvider;
 import io.github.jbellis.jvector.vector.types.VectorFloat;
-import io.github.jbellis.jvector.vector.types.VectorTypeSupport;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 import org.slf4j.Logger;
@@ -15,6 +29,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode(Mode.AverageTime)
@@ -24,37 +39,25 @@ import java.util.concurrent.TimeUnit;
 @Warmup(iterations = 2)
 @Measurement(iterations = 5)
 @Threads(1)
-public class RandomVectorsBenchmark {
-    private static final Logger log = LoggerFactory.getLogger(RandomVectorsBenchmark.class);
-    private static final VectorTypeSupport VECTOR_TYPE_SUPPORT = VectorizationProvider.getInstance().getVectorTypeSupport();
+public class StaticSetVectorsBenchmark {
+    private static final Logger log = LoggerFactory.getLogger(StaticSetVectorsBenchmark.class);
     private RandomAccessVectorValues ravv;
     private ArrayList<VectorFloat<?>> baseVectors;
     private ArrayList<VectorFloat<?>> queryVectors;
+    private ArrayList<Set<Integer>> groundTruth;
     private GraphIndexBuilder graphIndexBuilder;
     private GraphIndex graphIndex;
     int originalDimension;
-    @Param({"1000", "10000", "100000", "1000000"})
-    int numBaseVectors;
-    @Param({"10"})
-    int numQueryVectors;
 
     @Setup
     public void setup() throws IOException {
-        originalDimension = 128; // Example dimension, can be adjusted
-
-        baseVectors = new ArrayList<>(numBaseVectors);
-        queryVectors = new ArrayList<>(numQueryVectors);
-
-        for (int i = 0; i < numBaseVectors; i++) {
-            VectorFloat<?> vector = createRandomVector(originalDimension);
-            baseVectors.add(vector);
-        }
-
-        for (int i = 0; i < numQueryVectors; i++) {
-            VectorFloat<?> vector = createRandomVector(originalDimension);
-            queryVectors.add(vector);
-        }
-
+        var siftPath = "siftsmall";
+        baseVectors = SiftLoader.readFvecs(String.format("%s/siftsmall_base.fvecs", siftPath));
+        queryVectors = SiftLoader.readFvecs(String.format("%s/siftsmall_query.fvecs", siftPath));
+        groundTruth = SiftLoader.readIvecs(String.format("%s/siftsmall_groundtruth.ivecs", siftPath));
+        log.info("base vectors size: {}, query vectors size: {}, loaded, dimensions {}",
+                baseVectors.size(), queryVectors.size(), baseVectors.get(0).length());
+        originalDimension = baseVectors.get(0).length();
         // wrap the raw vectors in a RandomAccessVectorValues
         ravv = new ListRandomAccessVectorValues(baseVectors, originalDimension);
 
@@ -70,23 +73,16 @@ public class RandomVectorsBenchmark {
         graphIndex = graphIndexBuilder.build(ravv);
     }
 
-    private VectorFloat<?> createRandomVector(int dimension) {
-        VectorFloat<?> vector = VECTOR_TYPE_SUPPORT.createFloatVector(dimension);
-        for (int i = 0; i < dimension; i++) {
-            vector.set(i, (float) Math.random());
-        }
-        return vector;
-    }
-
     @TearDown
     public void tearDown() throws IOException {
         baseVectors.clear();
         queryVectors.clear();
+        groundTruth.clear();
         graphIndexBuilder.close();
     }
 
     @Benchmark
-    public void testOnHeapRandomVectors(Blackhole blackhole) {
+    public void testOnHeapWithRandomQueryVectors(Blackhole blackhole) throws IOException {
         var queryVector = SiftSmall.randomVector(originalDimension);
         // Your benchmark code here
         var searchResult = GraphSearcher.search(queryVector,
